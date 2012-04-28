93ff9a978256d6adadb5ae02d4a66cabbd959fe4
nrkn <nrkn.com@gmail.com>
2012-04-28 22:03:41 +1200

    moar cleaning up files

--- mus/starwars.js
+++
@@ -1,7 +0,0 @@
-var noteCompiler = require( './noteCompiler.js' ),
-    fs = require( 'fs' ),
-    data = fs.readFileSync( './data/starwars.mus', 'utf-8' );
-    
-var note = noteCompiler.compile( JSON.parse( data ) );    
-
-fs.writeFileSync( './data/starwars.note', JSON.stringify( note, null, 2 ) );
--- musick/starwars.js
+++
@@ -1,6 +0,0 @@
-var assert = require( 'assert' ),
-    musCompiler = require( './musCompiler' );
-    fs = require( 'fs' ),
-    data = fs.readFileSync( './data/starwars.musick', 'utf-8' );
-
-musCompiler.save( data, './data/starwars.mus' );

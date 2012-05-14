f2039be56698cf244402154d079224e22a33b872
nrkn <nrkn.com@gmail.com>
2012-05-14 20:13:18 +1200

    reorganised directories

--- musick/tests.js
+++
@@ -1,5 +0,0 @@
-var assert = require( 'assert' ),
-    musCompiler = require( './musCompiler' );
-    
-//basic case
-assert.deepEqual( musCompiler.compile( 'a' ), { "tag": "seq", "left": { "tag": "note", "dur": 437.5, "pitch": "a4" }, "right": { "tag": "rest", "dur": 62.5 } }  );
